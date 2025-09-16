a3eac91aa546169f07ff269917f369478f2d6869
Marcos Godinho Filho <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2025-09-16 05:16:14 -0300

    Update router.events.ts

--- src/routers/router.events.ts
+++ src/routers/router.events.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { 
+  addUserToEvent,
   createEvent, 
   finishEvent, 
   getEventById,
@@ -13,6 +14,8 @@ eventsRouter.get("/:eventId/get_event", getEventById)
 eventsRouter.get("/:userId/get_user_events", getEventsByUserId)
 
 eventsRouter.post("/events/create", createEvent);
+
+eventsRouter.post("/:eventId/add_user", addUserToEvent)
 
 eventsRouter.post("/:eventId/finish", finishEvent);
 
