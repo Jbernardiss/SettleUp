5ab286e423c5872f2455420bdebc5fbdc9355500
jbernardis <jbernardis@ciandt.com>
2025-09-15 22:28:19 -0300

    one more for the road

--- src/service/service.event.ts
+++ src/service/service.event.ts
@@ -157,17 +157,102 @@ export const answerExpenseNotification = async (req: Request, res: Response) => 
 };
 
 export const finishEvent = async (req: Request, res: Response) => {
-  try {
-    const { eventId } = req.params;
-    const eventRef = db.collection('events').doc(eventId);
 
-    await eventRef.update({
-      finished: true,
-    });
+    // Get event via id
+    const eventRef = db.collection('events').doc('190wma4uJJME5inVnzaD');
+    const doc = await eventRef.get();
+    const expenseRef = query(db.collection('expenses'), where('event', '==', '190wma4uJJME5inVnzaD'));
+    const event: Event = {
+      id: doc.id,
+      name: doc.data().name,
+      members: doc.data().members,
+      expenses: doc.data().expenses,
+      finished: doc.data().finished
+    }
 
-    res.status(200).json({ message: `Event ${eventId} has been marked as finished.` });
-  } catch (error) {
-    console.error('Error finishing event:', error);
-    res.status(500).json({ error: 'Failed to finish event.' });
-  }
-};
+
+
+    // Get transactions data
+    const server = new StellarSdk.Horizon.Server('https://horizon-testnet.stellar.org');
+    async function getTransactionData(expense: string) {
+      try {
+        const tx = await server.transactions().transaction(expense).call();
+        // console.log("Transaction Data:", tx);
+        console.log(`Source Account: ${tx.source_account}`)
+
+        let amount;
+        const { records: operations } = await tx.operations()
+        for (const op of operations) {
+          if (op.type === 'payment' && op.asset_type === 'native') {
+            amount = op.amount
+          }
+        }
+        return [tx.source_account, amount]
+      } catch (error) {
+        console.error("Error fetching transaction:", error);
+      }
+    }
+
+    let expensesData = {}
+    for(let expense of event.expenses) {
+      expensesData[expense] = getTransactionData(expense)
+    }
+
+
+    let individualTabs = {}
+    let individualTransactionsList = {}
+    for(let member of event.members) {
+        individualTabs[member] = []
+        individualTransactionsList[member] = []
+    }
+
+    for(let expense of event.expenses) {
+        let expenseData = expensesData[expense]
+        individualTabs[expenseData[0]].push(-(expenseData[1])); 
+    }
+    console.log(individualTabs)
+
+    let sum;
+    for(let member in individualTabs) {
+      sum = 0;
+      for(let value of individualTabs[member]) {
+        sum += value;
+      }
+    
+      individualTabs[member] = [sum]
+    }
+
+
+    for(let member in individualTabs) {
+      const split = individualTabs[member][0]/event.members.length
+      individualTabs[member].push(-(individualTabs[member][0] - split))
+
+      for(let otherMember in individualTabs) {
+        if (otherMember != member) {
+          individualTabs[otherMember].push(split)
+          individualTransactionsList[otherMember].push([member, split])
+        }
+      }
+    }
+
+    console.log(individualTransactionsList)
+    console.log(individualTabs)
+
+
+    try {
+      const { eventId } = req.params;
+      const eventRef = db.collection('events').doc(eventId);
+
+      await eventRef.update({
+        finished: true,
+      });
+
+      res.status(200).json({ message: `Event ${eventId} has been marked as finished.` });
+    } catch (error) {
+      console.error('Error finishing event:', error);
+      res.status(500).json({ error: 'Failed to finish event.' });
+    }
+
+    
+    res.send(`Event ${req.params.eventId} finished`);
+}
